030177a6b7a29e37f18a593928a4a64c4705c134
engIbrahemQruea <mmany6186@gmail.com>
2025-11-04 20:35:07 +0300

    add image  and assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+class ClsAppAssets {
+  static const String logoImage = 'assets/images/app_logo.png';
+  static const String noTasksImage = 'assets/images/no_task.png';
+  static const String onBoardingOneImage = 'assets/images/onboarding_one.png';
+  static const String onBoardingTwoImage = 'assets/images/onboarding_tow.png';
+  static const String onBoardingThreeImage = 'assets/images/onboarding_three.png';
+}
+
+
