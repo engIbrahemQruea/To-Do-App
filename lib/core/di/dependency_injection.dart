ba0b76a2b0b795f40e475d23a7cb2737ba95718e
engIbrahemQruea <mmany6186@gmail.com>
2025-11-06 00:41:26 +0300

    create get_it class

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupGetIt() async {
+
+
+  // // Dio & ApiService
+  // Dio dio = DioFactory.getDio();
+  // getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  // // login
+  // getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  // getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+
+  // // signup
+  // getIt.registerLazySingleton<SignupRepo>(() => SignupRepo(getIt()));
+  // getIt.registerFactory<SignupCubit>(() => SignupCubit(getIt()));
+
+  // // home
+  // getIt.registerLazySingleton<HomeApiService>(() => HomeApiService(dio));
+  // getIt.registerLazySingleton<HomeRepo>(() => HomeRepo(getIt()));
+}
